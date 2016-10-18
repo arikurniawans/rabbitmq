38a141cef4729684470d561f50d698ebee9432ea
Ari Kurniawan Saputra <ari.12421013@gmail.com>
2016-10-18 15:58:34 +0700

    LEARN RABBITMQ WITH JSONLD

---
+++ latrbjs/CommonService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace latrbjs
+{
+    class CommonService
+    {
+        private string _hostName = "167.205.66.183";
+        private string _userName = "gue";
+        private string _password = "gue";
+
+        public static string SerialisationQueueName = "lumen.json.lagi";
+
+        public IConnection GetRabbitMqConnection()
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory();
+            connectionFactory.HostName = _hostName;
+            connectionFactory.UserName = _userName;
+            connectionFactory.Password = _password;
+
+            return connectionFactory.CreateConnection();
+        }
+    }
+}
---
+++ latrbjs/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using Newtonsoft.Json;
+
+namespace latrbjs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommonService commonService = new CommonService();
+            IConnection connection = commonService.GetRabbitMqConnection();
+            IModel model = connection.CreateModel();
+            SetupSerialisationMessageQueue(model);
+            RunSerialisationDemo(model);
+        }
+
+        private static void SetupSerialisationMessageQueue(IModel model)
+        {
+            model.QueueDeclare(CommonService.SerialisationQueueName, true, false, false, null);
+        }
+
+        private static void RunSerialisationDemo(IModel model)
+        {
+            Console.WriteLine("Enter customer name. Quit with 'q'.");
+            while (true)
+            {
+                string customerName = Console.ReadLine();
+                if (customerName.ToLower() == "q") break;
+                Customer customer = new Customer() { Name = customerName };
+                IBasicProperties basicProperties = model.CreateBasicProperties();
+                basicProperties.SetPersistent(true);
+                String jsonified = JsonConvert.SerializeObject(customer);
+                byte[] customerBuffer = Encoding.UTF8.GetBytes(jsonified);
+                model.BasicPublish("", CommonService.SerialisationQueueName, basicProperties, customerBuffer);
+            }
+        }
+    }
+}
